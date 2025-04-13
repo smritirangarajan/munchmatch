43f74bf56e570daf826fa7d9858b8e892e5ec774
smritirangarajan <smriti_rangarajan@berkeley.edu>
2025-04-12 21:51:20 -0700

    Delete server directory

--- server/routes/foursquareRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {
-  findMatches
-} = require("../utils/foursquare");
-
-router.get("/find-matches", findMatches);
-
-module.exports = router;
